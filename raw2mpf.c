10ed9213956e817fc4d3c2964b9f0f4512dc91ec
Rik Snel <rik@snel.it>
2019-06-23 09:57:32 +0200

    refactor part 3

--- raw2mpf.c
+++ raw2mpf.c
@@ -29,7 +29,8 @@
 #define DURATION 0.000125 
 #define MAX_DATA_SIZE 8*1024 // 8kB
 
-typedef enum state_e { NONE, HEADER, MID_SYNC, FIRST_DATA, DATA, TAIL_SYNC } state_t;
+typedef enum state_e { NONE, HEADER, HEADER_SEPARATOR, MID_SYNC,
+	FIRST_DATA, DATA, DATA_SEPARATOR, TAIL_SYNC } state_t;
 
 const char *state_string(state_t state) {
 	switch (state) {
@@ -39,6 +40,9 @@ const char *state_string(state_t state) {
 		case HEADER:
 			return "HEADER";
 			break;
+		case HEADER_SEPARATOR:
+			return "HEADER_SEPARATOR";
+			break;
 		case MID_SYNC:
 			return "MID_SYNC";
 			break;
@@ -47,6 +51,9 @@ const char *state_string(state_t state) {
 			break;
 		case DATA:
 			return "DATA";
+			break;
+		case DATA_SEPARATOR:
+			return "DATA_SEPARATOR";
 			break;
 		case TAIL_SYNC:
 			return "TAIL_SYNC";
@@ -71,151 +78,98 @@ const char *run_string(run_t run) {
 	}
 }
 
-void decode_inner(run_t run, int length) {
+// this macro is used in decode() and in decode_inner()
+#define ODD (count%2)
+
+void decode_inner(run_t run, int length, int polarity) {
+	static int count = 0;
 	static state_t state = NONE;
-	static int bytes, bits, bit, data_size;
-	static run_t last_run = SHORT, last_length = 0;
+	static int bits, bit, data_size;
+	static run_t last_run = LONG, last_length = 1;
 	static unsigned char header[7], data[MAX_DATA_SIZE], checksum;
 	static unsigned short first_addr, filename;
+	static unsigned char *ptr, *end_ptr;
+
+	count++;
+
+	// this function is always called with alternating values of
+	// run, the code below this assert() relies on it
+	assert(last_run != run);
 
 	// according to MPF-1 monitor source listing: LEAD_SYNC is
-	// accepted if it is longer than 1 second
-	// a LEAD_SYNC always resets the state machine
+	// accepted if it is longer than 1 second a LEAD_SYNC always
+	// resets the state machine
 	if (run == LONG && length > 1000) {
 		if (state != NONE) ERROR("LEAD_SYNC found while in state %s", state_string(state));
-		bytes = 7; // expecting 7 bytes
+		ptr = header;
+		end_ptr = header + 7; // expecting 7 bytes
 		bits = -1; // expecting bit 0 (startbit)
-		state = HEADER;
+		state = HEADER_SEPARATOR;
 		memset(header, 0, sizeof(header));
 		memset(data, 0, sizeof(data));
-		VERBOSE("found LEAD_SYNC, loading HEADER");
+		VERBOSE("found %.1fs LEAD_SYNC, loading HEADER, %s polarity", length/1000., polarity?"positive":"negative");
 	} else switch (state) {
 		case NONE:
-			// if we are in state NONE and there is no LEAD_SYNC
-			// then we stay in state NONE
+			/* if we are in state NONE and there is no LEAD_SYNC
+			 * then we stay in state NONE */
 			break;
-		case HEADER:
-			if (run == LONG) {
+		case FIRST_DATA: /* called with LONG run */
+			/* the SHORT run of FIRST_DATA is contained in the
+			 * MID_SYNC
+			 * correct length based on length of current LONG run, 
+			 * update state and fall through
+			 *
+			 * if length == 2, then last_length will be set to 8
+			 * if length == 4, then last length will be set to 4
+			 * otherwise both will be invalid */
+			last_length = -2*length + 12;
+			state = DATA;
+			/* fall through to DATA */
+		case DATA:   /* called with LONG run */
+		case HEADER: /* called with LONG run */
+			if (length == 2 && last_length == 8) bit = 0;
+			else if (length == 4 && last_length == 4) bit = 1;
+			else {
+				ERROR("invalid bit found in state %s length = %d, last_length = %d", state_string(state), length, last_length);
+				state = NONE;
+				break;
+			}
+			bits++;
+			if (bits == 0) {
+				// start bit should be 0
+				if (bit != 0) WARNING("invalid start bit found, must be 0 found 1");
+			} else if (bits == 9) {
+				if (state == DATA) checksum -= *ptr;
+				// stop bit should be 1
+				if (bit != 1) WARNING("invalid stop bit found, must be 1 found 0");
+				ptr++;
+				bits = -1;
+			} else {
+				*ptr >>= 1;
+				if (bit) *ptr |= 0x80;
+			}
 
-				if (length == 2 && last_run == SHORT && last_length == 8) {
-					bit = 0;
-				} else if (length == 4 && last_run == SHORT && last_length == 4) {
-					bit = 1;
-				} else {
-					ERROR("invalid bit found in state HEADER length = %d, last_length = %d", length, last_length);
-					state = NONE;
-					break;
-				}
-				bits++;
-				if (bits == 0) {
-					// start bit must be 0
-					if (bit != 0) {
-						ERROR("invalid start bit found, must be 0 found 1");
-						state = NONE;
-						break;
-					}
-				} else if (bits == 9) {
-					//VERBOSE("read header byte %02x", header[7-bytes]);
-					//printf("%02x\n", header[7-bytes]);
-					// stop bit must be 1
-					if (bit != 1) {
-						ERROR("invalid stop bit found, must be 1 found 0");
-						state = NONE;
-						break;
-					}
-					bytes--;
-					bits = -1;
-				} else {
-					header[7 - bytes] >>= 1;
-					if (bit) header[7 - bytes] |= 0x80;
-				}
-
-				if (bytes == 0) {
+			if (state == HEADER) {
+				if (ptr == end_ptr) {
 					unsigned short last_addr;
 					filename = le16toh(*(unsigned short*)&header[0]);
 					first_addr = le16toh(*(unsigned short*)&header[2]);
 					last_addr = le16toh(*(unsigned short*)&header[4]);
 					checksum = header[6];
-					data_size = bytes = last_addr - first_addr + 1;
-					if (bytes > MAX_DATA_SIZE) {
-						ERROR("data size %d > %d bytes, not supported; increase MAX_DATA_SIZE", bytes, MAX_DATA_SIZE);
+					data_size = last_addr - first_addr + 1;
+					if (data_size > MAX_DATA_SIZE) {
+						ERROR("data size %d > %d bytes, not supported; increase MAX_DATA_SIZE", data_size, MAX_DATA_SIZE);
 						state = NONE;
 						break;
 					}
+					ptr = data;
+					end_ptr = data + data_size;
 					VERBOSE("header: filename=%04x, first_addr=%04x, last_addr=%04x, checksum=%02x", filename, first_addr, last_addr, checksum);
 					state = MID_SYNC;
 					assert(bits == -1);
-				}
-			}
-			break;
-		case MID_SYNC:
-			// mid sync must be at least 1.5 seconds, so that the
-			// MFP-1 can display the filename for 1.5 seconds before
-			// reading the data
-			if (run == SHORT && length > 3000) { 
-				state = FIRST_DATA;
-				VERBOSE("found MID_SYNC, loading DATA (%d bytes)", data_size);
-			} else {
-				ERROR("invalid run in MID_SYNC");
-				state = NONE;
-			}
-			break;
-		case FIRST_DATA:
-			// the SHORT run of FIRST_DATA is contained in the
-			// MID_SYNC
-			// correct length based on length of current LONG run, 
-			// update state and fall through
-			if (run == LONG) {
-				if (length == 4) {
-					last_length = 4;
-				} else if (length == 2) {
-					last_length = 8;
-				} else {
-					ERROR("invalid bit found in state FIRST_DATA");
-					state = NONE;
-					break;
-				}
-			} else {
-				ERROR("invalid run in FIRST_DATA");
-				state = NONE;
-				break;
-			}
-			state = DATA;
-		case DATA:
-			if (run == LONG) {
-				if (length == 2 && last_run == SHORT && last_length == 8) {
-					bit = 0;
-				} else if (length == 4 && last_run == SHORT && last_length == 4) {
-					bit = 1;
-				} else {
-					ERROR("invalid bit found in state HEADER");
-					state = NONE;
-					break;
-				}
-				bits++;
-				if (bits == 0) {
-					// start bit must be 0
-					if (bit != 0) {
-						ERROR("invalid start bit found, must be 0 found 1");
-						state = NONE;
-						break;
-					}
-				} else if (bits == 9) {
-					checksum -= data[data_size-bytes];
-					//printf("%02x\n", data[data_size-bytes]);
-					// stop bit must be 1
-					if (bit != 1) {
-						ERROR("invalid stop bit found, must be 1 found 0");
-						state = NONE;
-						break;
-					}
-					bytes--;
-					bits = -1;
-				} else {
-					data[data_size - bytes] >>= 1;
-					if (bit) data[data_size - bytes] |= 0x80;
-				}
-				if (bytes == 0) {
+				} else state = HEADER_SEPARATOR;
+			} else if (state == DATA) {
+				if (ptr == end_ptr) {
 					if (checksum != 0x00) {
 						ERROR("invalid checksum");
 						state = NONE;
@@ -227,19 +181,42 @@ void decode_inner(run_t run, int length) {
 					printf("\n");
 					fflush(stdout);
 					state = TAIL_SYNC;
-				}
+				} else state = DATA_SEPARATOR;
 			}
 			break;
-		case TAIL_SYNC:
-			// the purpose of tail sync is not very clear, let's say
-			// it needs to be at lease 0.5 seconds long to be valid
-			if (run == SHORT && length > 1000) { 
-				VERBOSE("found TAIL_SYNC");
-				state = NONE;
+		case HEADER_SEPARATOR: /* called with SHORT run */
+			state = HEADER;
+			break;
+		case MID_SYNC: /* called with SHORT run */
+			// mid sync must be at least 1.5 seconds, so that the
+			// MFP-1 can display the filename for 1.5 seconds before
+			// reading the data
+			//
+			// strictly speaking the length of MID_SYNC can't be
+			// known at this point, because the last 4 or 8 waves
+			// belong to the bit following the sync
+			if (length >= 2900) {  /* use this, so that if the duration is
+						* shorter, the time in seconds
+						* rounded to one decimal is
+						* actually smaller than 1.5s,
+						* otherwise the error message
+						* may not make sense */
+				state = FIRST_DATA;
+				VERBOSE("found %.1fs MID_SYNC, loading DATA (%d bytes)", length/2000., data_size);
 			} else {
-				ERROR("invalid run in TAIL_SYNC");
+				ERROR("duration of MID_SYNC is too short %.1fs < %.1fs", length/2000., 1.5);
 				state = NONE;
 			}
+			break;
+		case DATA_SEPARATOR: /* called with SHORT run */
+			state = DATA;
+			break;
+		case TAIL_SYNC: /* called with SHORT run */
+			// the purpose of tail sync is not very clear, let's say
+			// it needs to be at lease 0.5 seconds long to be valid
+			if (length >= 900) VERBOSE("found %.fs TAIL_SYNC", length/2000.);
+			else ERROR("duration of TAIL_SYNC is too short %.1fs < %.1fs", length/2000., 1.5);
+			state = NONE;
 			break;
 	}
 
@@ -247,7 +224,6 @@ void decode_inner(run_t run, int length) {
 
 }
 
-#define ODD (count%2)
 // the first time this function is called, it is notified
 // of a rising edge, the second time of a falling edge etc
 void decode(double duration) {
@@ -256,7 +232,7 @@ void decode(double duration) {
 	// and if (postincrement) count is even, then we were called at a
 	// falling edge
 	static int count = 0;
-	int phase;
+	int polarity;
 	static int length = 0;
 	static double periods[2] = { 0, 0 }, durations[2] = { 0, 0 }, goodnesses[2] = { 0, 0 };
 	static run_t run = SHORT;
@@ -275,9 +251,9 @@ void decode(double duration) {
 	 * if the phase is correctly interpreted, only
 	 * periods of .5 and 1ms are detected
 	 *
-	 * in the below illustration, the phase is negative ( \ first )
+	 * in the below illustration, the polarity is negative ( \ first )
 	 *
-	 *  wrong phase interpretation
+	 *  wrong polarity
 	 *
 	 *      .75ms       .75m       .5ms     .75ms          1ms
 	 *  +-----------+-----------+-------+-----------+----------------+
@@ -289,13 +265,17 @@ void decode(double duration) {
 	 *  ----+---------------+-------+-------+----------------+--------
 	 *             1ms        .5ms    .5ms         1ms
 	 *
-	 * correct phase interpretation
+	 * correct polarity
 	 *
 	 * the goodness of a period is a measure of
 	 * the distance from 75ms
 	 *
 	 * in this way this program attempts to guess
-	 * the correct polarity every time....  */
+	 * the correct polarity every time.... 
+	 *
+	 * Why not just use one side (up or down) and multiply by 2 to get the
+	 * duration? This only works if the signal is sufficiently symmetrictal
+	 * and it turned out that, in practice, that is not true */
 	goodnesses[ODD] = fabs(log(periods[ODD]/0.00075));
 
 	/* only every second call to this function
@@ -303,16 +283,17 @@ void decode(double duration) {
 
 	if (ODD) return;
 
-	if (goodnesses[0] > goodnesses[1]) phase = 0;
-	else phase = 1;
+	if (goodnesses[0] > goodnesses[1]) polarity = 0;
+	else polarity = 1;
 
-	cur_run = (periods[phase] < 0.00075)?SHORT:LONG;
+	cur_run = (periods[polarity] < 0.00075)?SHORT:LONG;
 
 	length++; // increment length of current run
 
 	if (cur_run == run) return;
 
-	decode_inner(run, length);
+	decode_inner(run, length, polarity);
+
 	run = cur_run;
 	length = 0;
 }
